44230b6354781ddfcefa4f45c87001978256cdc6
Dave Yan <fdu5526@gmail.com>
2015-05-01 02:03:14 -0400

    starting to time dialogues

--- I_Lie_Awake/src/setupDayOneDialogue.js
+++ I_Lie_Awake/src/setupDayOneDialogue.js
@@ -20,8 +20,9 @@ var choicesDad = [
 ];
 
 var dadTalks = [
-	new Phrase("Dad", "Have a safe trip to school :)", 100),
-	new Phrase("Dad", "Morning sweetie, I left some breakfast on the table", 100),
+	new Phrase("Dad", "", 2500),
+	new Phrase("Dad", "Morning sweetie, I left some breakfast on the table", 3000),
+	new Phrase("Dad", "Have a safe trip to school", 2500),
 	new Phrase("Dad", "Don't wake mom, she still needs her rest", choicesDad)
 ];
 
@@ -32,12 +33,12 @@ var dialogueWithDad = new Dialogue(dadTalks);
 // Walk to School
 
 var johnnyShucks = [
-	new Phrase("Johnny", "awww shucks", 100),
-	new Phrase("Johnny", "see you at school", 100),
+	new Phrase("Johnny", "awww shucks", 4000),
+	new Phrase("Johnny", "see you at school", 4000),
 ];
 
 var johnnyNoWay = [
-	new Phrase("Johnny", "dude no wayyyyyyyyy :O", 100)
+	new Phrase("Johnny", "dude no wayyyyyyyyy :O", 4000)
 ];
 
 var choices2 = [
@@ -53,17 +54,17 @@ var choices1 = [
 ];
 
 var walkToSchool = [
-	new Phrase("Mom", "Did you leave already?", 100),
-	new Phrase("Mom", "I am worried about the situations recently", 100),
-	new Phrase("Mom", "Stay safe, don't go looking for trouble", 100),
-	new Phrase("Johnny", "yooo coming to school today?", 100),
-	new Phrase("Rachel", "i am! where are you guys?", 100),
-	new Phrase("Johnny", "already hereeee", 100),
-	new Phrase("Sarah", "I will be there soon...", 100),
+	new Phrase("Mom", "Did you leave already?", 3000),
+	new Phrase("Mom", "I am worried about the situations recently", 4000),
+	new Phrase("Mom", "Stay safe, don't go looking for trouble", 4500),
+	new Phrase("Johnny", "yooo coming to school today?", 3000),
+	new Phrase("Rachel", "i am! where are you guys?", 1500),
+	new Phrase("Johnny", "already hereeee", 1000),
+	new Phrase("Sarah", "I will be there soon...", 4000),
 	new Phrase("Mom", "Try to avoid crowds and strangers", choices1),
-	new Phrase("Rachel", "omw there are soldiers everywhere!", 100),
-	new Phrase("Johnny", "brenda just told me he saw a jeep with a big gun on it!", 100),
-	new Phrase("Andy", "No way brenda is such a liar.", 100),
+	new Phrase("Rachel", "omw there are soldiers everywhere!", 3000),
+	new Phrase("Johnny", "brenda just told me he saw a jeep with a big gun on it!", 3000),
+	new Phrase("Andy", "No way brenda is such a liar.", 2000),
 	new Phrase("Johnny", "have you seen anything cool yet?", choices2)
 ];
 
@@ -79,9 +80,9 @@ var choices3 = [
 ];
 
 var walkFromSchool = [
-	new Phrase("Mom", "I just remembered", 100),
-	new Phrase("Mom", "Can you pick up a dozen eggs from the grocery store today?", 100),
-	new Phrase("Rachel", "hey you coming to the park? :)", 100),
+	new Phrase("Mom", "I just remembered", 1000),
+	new Phrase("Mom", "Can you pick up a dozen eggs from the grocery store today?", 5000),
+	new Phrase("Rachel", "hey you coming to the park? :)", 2000),
 	new Phrase("Rachel", "we are all going to play hide and seek today! :D", choices3)
 ];
 
@@ -91,10 +92,10 @@ dialogueFromSchool.onEnd = defaults.goToHome;
 // Walk to home
 
 var walkToHome = [
-	new Phrase("Johnny", "ttyl dinner with familyyyy", 100),
-	new Phrase("Rachel", "bye bye :)", 100),
-	new Phrase("Brenda", "c u tommorw", 100),
-	new Phrase("Sarah", "Be careful on your way home.", 100)
+	new Phrase("Johnny", "ttyl dinner with familyyyy", 2000),
+	new Phrase("Rachel", "bye bye :)", 1000),
+	new Phrase("Brenda", "c u tommorw", 1500),
+	new Phrase("Sarah", "Be careful on your way home.", 4000)
 ];
 
 var dialogueToHome = new Dialogue(walkToHome);
