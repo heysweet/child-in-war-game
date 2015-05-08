ee7558a574d57c228f1a415bff71176de4e5ee68
Dave Yan <fdu5526@gmail.com>
2015-05-08 15:58:04 -0400

    finished day 1 dialogue

--- I_Lie_Awake/src/setupDayOneDialogue.js
+++ I_Lie_Awake/src/setupDayOneDialogue.js
@@ -16,12 +16,12 @@ var defaults = require("dialogueDefaults.js");
 
 // Dad Morning
 var choicesDad = [
-	new Choice("ok", null)
+	new Choice("yeah ok", null),
+	new Choice("sure", null)
 ];
 
 var dadTalks = [
-	new Phrase("Dad", "Morning sweetie, I left some breakfast on the table", 3000),
-	new Phrase("Dad", "Have a safe trip to school", 2500),
+	new Phrase("Dad", "Morning sweetie, Have a safe trip to school", 3000),
 	new Phrase("Dad", "Don't wake mom, she still needs her rest", choicesDad)
 ];
 
@@ -31,40 +31,57 @@ var dialogueWithDad = new Dialogue(dadTalks);
 
 // Walk to School
 
-var johnnyShucks = [
-	new Phrase("Johnny", "awww shucks", 4000),
-	new Phrase("Johnny", "see you at school", 4000),
+var johnnyBoring = [
+	new Phrase("Johnny", "booorrrrriiinnnnnnnggggggggggggg", 4000),
 ];
 
 var johnnyNoWay = [
-	new Phrase("Johnny", "dude no wayyyyyyyyy :O", 4000)
+	new Phrase("Johnny", "dude no wayyyyyyyyy", 4000)
+];
+
+
+var choices1 = [
+	new Choice("yeah whatever", null),
+	new Choice("dont worry mom!", null),
 ];
 
 var choices2 = [
-	new Choice("just some soldiers, nothing exciting", johnnyShucks),
-	new Choice("oh yeah? i saw a tank", johnnyNoWay)
+	new Choice("getting there sheesh", null),
+	new Choice("on my way", null),
 ];
 
-	// null will make sure it jumps back into the next phrase in the main dialogue
-	// Whenever a dialogue path
-var choices1 = [
-	new Choice("don't worry mom, i'll be ok", null),
-	new Choice("you worry too much mom", null)
+var choices3 = [
+	new Choice("you are so slow wow", null),
+	new Choice("see you there!", null),
+];
+
+var choices4 = [
+	new Choice("you worry too much mom", null),
+	new Choice("no problem!", null),
+];
+
+var choices5 = [
+	new Choice("thats cool", null),
+	new Choice("thats scary", null),
+];
+
+var choices6 = [
+	new Choice("just some soldiers, nothing exciting", johnnyBoring),
+	new Choice("oh yeah? i saw a tank", johnnyNoWay)
 ];
 
 var walkToSchool = [
 	new Phrase("Mom", "Did you leave already?", 3000),
-	new Phrase("Mom", "I am worried about the situations recently", 4000),
-	new Phrase("Mom", "Stay safe, don't go looking for trouble", 4500),
+	new Phrase("Mom", "Stay safe, don't go looking for trouble", choices1),
 	new Phrase("Johnny", "yooo coming to school today?", 3000),
-	new Phrase("Rachel", "i am! where are you guys?", 1500),
+	new Phrase("Rachel", "i am! where are you guys?", choices2),
 	new Phrase("Johnny", "already hereeee", 1000),
-	new Phrase("Sarah", "I will be there soon...", 4000),
-	new Phrase("Mom", "Try to avoid crowds and strangers", choices1),
+	new Phrase("Sarah", "I will be there soon...", choices3),
+	new Phrase("Mom", "Try to avoid crowds and strangers", choices4),
 	new Phrase("Rachel", "omw there are soldiers everywhere!", 3000),
-	new Phrase("Johnny", "Sam just told me she saw a jeep with a big gun on it!", 3000),
+	new Phrase("Johnny", "Sam just told me she saw a jeep with a big gun on it!", choices5),
 	new Phrase("Andy", "No way Sam is such a liar.", 2000),
-	new Phrase("Johnny", "have you seen anything cool yet?", choices2)
+	new Phrase("Johnny", "have you seen anything cool yet?", choices6)
 ];
 
 var dialogueToSchool = new Dialogue(walkToSchool);
@@ -73,7 +90,7 @@ dialogueToSchool.onEnd = defaults.goToSchool;
 
 // Walk from School
 
-var choices3 = [
+var choices7 = [
 	new Choice("kk i'll be right there", null, "Friends"),
 	new Choice("sorry but i got to buy eggs for mom today", null, "Parents")
 ];
@@ -82,7 +99,7 @@ var walkFromSchool = [
 	new Phrase("Mom", "I just remembered", 1000),
 	new Phrase("Mom", "Can you pick up a dozen eggs from the grocery store today?", 5000),
 	new Phrase("Rachel", "hey you coming to the park? :)", 2000),
-	new Phrase("Rachel", "we are all going to play hide and seek today! :D", choices3)
+	new Phrase("Rachel", "we are all going to play hide and seek today! :D", choices7)
 ];
 
 var dialogueFromSchool = new Dialogue(walkFromSchool);
