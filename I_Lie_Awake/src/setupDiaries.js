d87c4086574c4563c3fd42bf88d7574b54e6325d
Dave Yan <fdu5526@gmail.com>
2015-05-08 16:41:30 -0400

    day 2 & 3 and diary entries done

--- I_Lie_Awake/src/setupDiaries.js
+++ I_Lie_Awake/src/setupDiaries.js
@@ -1,15 +1,15 @@
 var homeParents = [
 	"I got eggs for mom today, she is happy.\n\n" +
-"I didnt get to see my friends after school.\n\n" +
-"I wonder if they had fun without me.\n\n" +
-"Dad came back late at night, I was really happy to see him.",
+	"I didnt get to see my friends after school.\n\n" +
+	"I wonder if they had fun without me.\n\n" +
+	"Dad came back late at night, I was really happy to see him.",
 
 	"",
 
 	"I got water for mom today.\n\n" +
-"I hope I didn't miss out on too much fun with my friends.\n\n" +
-"I wonder what happened to Andy.\n\n" +
-"Dad did not tuck me in before I went to sleep.",
+	"I hope I didn't miss out on too much fun with my friends.\n\n" +
+	"I wonder what happened to Andy.\n\n" +
+	"Dad did not tuck me in before I went to sleep.",
 
 	"",
 
@@ -18,16 +18,16 @@ var homeParents = [
 
 var homeFriends = [
 	"I played hide and seek with friends today.\n\n" +
-"I had a lot of fun! I stayed out too long.\n\n" +
-"Mom got angry that I didnt get her eggs.\n\n" +
-"Dad came back late at night, he was really tired.",
+	"I had a lot of fun! I stayed out too long.\n\n" +
+	"Mom got angry that I didnt get her eggs.\n\n" +
+	"Dad came back late at night, he was really tired.",
 
 	"",
 
 	"I had a lot of fun playing with my friends today.\n\n" +
-"Mom got angry that I didnt get her water.\n\n"+
-"I wonder what happened to Andy.\n\n" +
-"Dad did not tuck me in before I went to sleep.",
+	"I wonder what happened to Andy\n\n"+
+	"I came home thirsty but there was no water to drink\n\n" +
+	"Mom and Dad did not tuck me in before I went to sleep",
 
 	"",
 
@@ -35,29 +35,34 @@ var homeFriends = [
 ];
 
 var school = [
-	"Ms. Beth taught us about rocks today. It is really boring, but Andy really liked it.\n\n" +
+	"Ms. Beth taught us about rocks today. It was really boring, but Andy really liked it.\n\n" +
 
-"Johnny and Sam are really excited about the soldiers, it's all they talked about during lunch.\n\n" +
+"Johnny and Sam were really excited about the soldiers, it's all they talked about during lunch.\n\n" +
 
 "Sarah seems scared though.",
+
 
 	"Ms. Beth taught us about mollusks in class. Mollusks is such a weird word.\n\n" +
 
 "She told us to be careful after school.\n\n" +
 
-"Sarah seems angry at Jonny and Sam. I did not see Rachel though.",
+"Sarah didn’t talk much today because Rachel was not here.",
+
+
 
 	"Ms. Beth taught about dolphins today. Dolphins are very weird animals that see with sound.\n\n" +
 
-"Johnny didn't talk much today because Sam was not here.\n\n" +
+"She warned us about what kind of things we should watch out for to avoid danger.\n\n" +
 
-"Sarah seemed very worried.",
+"Sarah was really angry at Sam, Johnny and me for bullying her.",
 
-	"Ms.Beth talked a lot about the war today. I didn't understand what she said.\n\n" + 
 
-"I had lunch with only Johnny. He didn't talk a lot.\n\n" +
 
-"I wonder if Mom found Dad yet.",
+	"Ms.Beth talked a lot about the war today. I didn’t understand what she said.\n\n" + 
+
+"I had lunch by myself today. There was no one to talk to.\n\n" +
+
+"I wonder where mom and dad went.",
 
 	"",
 ];
