18d1999694346f141ee84db10c33608239c7bba0
Andrew Sweet <andrewmsweet@gmail.com>
2015-05-09 02:36:01 -0400

    Using the appropriate apostrophe in the dialogue

--- I_Lie_Awake/src/setupDayTwoDialogue.js
+++ I_Lie_Awake/src/setupDayTwoDialogue.js
@@ -45,11 +45,11 @@ var choices5 = [
 walkToSchool = [
 	new Phrase("Mom", "Good morning honey.", 2000),
 	new Phrase("Mom", "What did Dad tell you?", choices1),
-	new Phrase("Mom", "Don’t worry about it. it is not a big deal.", 2000),
+	new Phrase("Mom", "Don't worry about it. it is not a big deal.", 2000),
 	new Phrase("Johnny", "i heard jet planes flyingggg last nightttttt", 2000),
 	new Phrase("Sam", "I heard them and explosions too!", choices2),
 	new Phrase("Sarah", "Mom said west side of town got bombed.", 2000),
-	new Phrase("Andy", "That’s pretty scary...", choices3),
+	new Phrase("Andy", "That's pretty scary...", choices3),
 	new Phrase("Mom", "How are your friends doing?", choices4),
 	new Phrase("Mom", "Well make sure to keep your guard up.", 2000),
 	new Phrase("Mom", "Be safe.", 2000),
